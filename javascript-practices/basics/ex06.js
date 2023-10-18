4e27ae37c8ad7815a4bf28ac49cd0f1afcf16d10
ryujiung <rju1202@naver.com>
2023-10-18 14:43:46 +0900

    object #2

--- javascript-practices/basics/ex06.js
+++ javascript-practices/basics/ex06.js
@@ -7,10 +7,35 @@
 
 // 생성 방법1
 // {} literal을 사용하는 방법
+var o2 = {};
+o2.name = '둘리';
+o2.age = 10;
+o2.another = new Object();
+o2.another.name = '마이콜';
+o2.another.age = 30;
+console.log(o2);
 
 // 생성 방법2
 // 1. Number, object, String ... 내장 객체(내장 생성자 함수)를 사용하는 방법
 // 2. 사용자 정의 생성자 함수를 사용하는 방법
+var o1 = new Object();
+o1.name = '둘리';
+o1.age = 10;
+o1.another = new Object();
+o1.another.name = '마이콜';
+o1.another.age = 30;
 
+console.log(o1);
 // 생성 방법3
 // JavaScriptObjectNotation -> JSON
+var o3 = {
+    name: '둘리',
+    age: 10,
+    another: {
+        name: '마이콜',
+        age: 30
+    }
+};
+console.log(o3);
+
+//XmlHttpRequest, fetch
